438ebcf35e77f590f6e617e2af0aa4ae92fc1aaf
Chris <chris.lovering.95@gmail.com>
2020-11-15 18:09:14 +0000

    Simpler implementation of local file detection

--- download_poe_tools.py
+++ download_poe_tools.py
@@ -20,22 +20,13 @@ projects_we_want = [
     {'id': 5, 'account': 'viktorgullmark', 'project': 'exilence-next'},
 ]
 
-def get_set_event_loop():
-    try:
-        return asyncio.get_event_loop()
-    except RuntimeError as e:
-        if e.args[0].startswith('There is no current event loop'):
-            asyncio.set_event_loop(asyncio.new_event_loop())
-            return asyncio.get_event_loop()
-        raise e
-
 def fetch_one(session, project, timeout=3, retries=0, retry_limit=3, headers=None):
     url = f'https://api.github.com/repos/{project["account"]}/{project["project"]}/releases/latest'
     try:
         with session.get(url, timeout=timeout, headers=headers) as response:
             response.raise_for_status()
             data = response.json()
-            project['version'] = data['tag_name']
+            project['latest_version'] = data['tag_name']
             # Find the first asset that's either .exe or .jar
             asset_we_want = next(item for item in data['assets'] if item['name'].endswith(('.exe', '.jar')))
             project['asset_download'] = asset_we_want['browser_download_url']
@@ -50,7 +41,7 @@ def fetch_one(session, project, timeout=3, retries=0, retry_limit=3, headers=Non
 async def get_data_asynchronous(headers=None):
     with ThreadPoolExecutor(max_workers=10) as executor:
         with requests.Session() as session:
-            loop = get_set_event_loop()
+            loop = asyncio.get_event_loop()
             tasks = [
                 loop.run_in_executor(
                     executor,
@@ -80,16 +71,17 @@ def download_file(url, file_save_path):
                 
                 print(f'[{done}{not_done}] {percent_done:.2%}',end='\r',flush=True)
         print() # escape carridge escaped line
-    return (time.perf_counter() - start) #time taken to download
+    return (time.perf_counter() - start)
 
 def get_installed_versions():
     with open(downloaded_files_info) as f:
         apparent_downloaded = json.load(f)
-    actual_downloaded = []
+    # for each file, check if it still exists
+    # if it does, add the installed version to projects_we_want
     for project in apparent_downloaded:
         if Path(downloads_dir, project['asset_name']).exists():
-            actual_downloaded.append(project)
-    return actual_downloaded
+            if remote_proj:=next((item for item in projects_we_want if item['id'] == project['id']), None):
+                remote_proj['installed_ver'] = project['installed_ver']
 
 
 if __name__ == '__main__':
@@ -97,35 +89,28 @@ if __name__ == '__main__':
     downloads_dir.mkdir(parents=True, exist_ok=True)
 
     if downloaded_files_info.exists():
-        installed_projects = get_installed_versions()
-    else:
-        installed_projects = None
+        get_installed_versions()
 
     # Get current versions asynchronously
-    loop = get_set_event_loop()
+    loop = asyncio.get_event_loop()
     verion_numbers_future = asyncio.ensure_future(get_data_asynchronous(
         headers={'Accept': 'application/json'}
     ))
     projects_we_want = loop.run_until_complete(verion_numbers_future)
 
     for project in projects_we_want:
-        print(f'Latest version of {project["project"]} is {project["version"]}')
+        print(f'Latest version of {project["project"]} is {project["latest_version"]}')
 
-        if installed_projects:
-            installed_ver = next((item for item in installed_projects if item["id"] == project['id']), None)
-            if installed_ver:
-                if installed_ver['version'] == project["version"]:
-                    print(f'Latest version already installed, skipping...')
-                    print() # Seperation
-                    continue
-                else:
-                    print(f'Found old version ({installed_ver}). ', end='', flush=True)
+        if project.get('installed_ver', None) == project["latest_version"]:
+            print(f'Latest version already installed, skipping...')
+        else:
+            file_save_path = Path(downloads_dir, project['asset_name'])
+            print(f'Downloading {project["asset_name"]}')
+            time_elapsed = download_file(project['asset_download'], file_save_path)
+            print(f'Finished downloading. Time taken: {round(time_elapsed, 2)}')
 
-        file_save_path = Path(downloads_dir, project['asset_name'])
-        print(f'Downloading {project["asset_name"]}')
-        time_elapsed = download_file(project['asset_download'], file_save_path)
-        print(f'Finished downloading. Time taken: {round(time_elapsed, 2)}')
         print() # Seperation
+        del project["latest_version"] # Remove this so that it isn't included in local file, causing confusion
     
     with open(downloaded_files_info, 'w') as f:
         json.dump(projects_we_want, f, indent=4)
