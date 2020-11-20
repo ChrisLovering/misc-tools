2839ef95ba29d208efabc20644f8acb44a6cd475
Chris <chris.lovering.95@gmail.com>
2020-11-20 18:25:46 +0000

    update comments

--- CopyStart.ps1
+++ CopyStart.ps1
@@ -1,5 +1,5 @@
-// From https://gist.github.com/YR-ZR0/d88a9f88d0052d835aeda2700ff2f05e
-// Script to Copy shortcuts to Startup Folder
+# From https://gist.github.com/YR-ZR0/d88a9f88d0052d835aeda2700ff2f05e
+# Script to Copy shortcuts to Startup Folder
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
  $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
  $OpenFileDialog.ShowDialog()|Out-Null
