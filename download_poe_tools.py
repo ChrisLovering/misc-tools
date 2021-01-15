52210006cfe9de845446473c47a3f99c71c1cfbe
Chris <chris.lovering.95@gmail.com>
2021-01-15 17:33:01 +0000

    linting

--- download_poe_tools.py
+++ download_poe_tools.py
@@ -21,18 +21,25 @@ projects_we_want = [
     {'id': 5, 'account': 'viktorgullmark', 'project': 'exilence-next'},
 ]
 
+
 def fetch_one(session, project, timeout=3, headers=None):
-    url = f'https://api.github.com/repos/{project["account"]}/{project["project"]}/releases/latest'
+    url = f'https://api.github.com/repos/{project["account"]}/'
+    f'{project["project"]}/releases/latest'
     with session.get(url, timeout=timeout, headers=headers) as response:
         response.raise_for_status()
         data = response.json()
         project['latest_version'] = data['tag_name']
         # Find the first asset that's either .exe or .jar
-        asset_we_want = next(item for item in data['assets'] if item['name'].endswith(('.exe', '.jar')))
+        asset_we_want = next(
+            item
+            for item in data['assets']
+            if item['name'].endswith(('.exe', '.jar'))
+        )
         project['asset_download'] = asset_we_want['browser_download_url']
         project['asset_name'] = asset_we_want['name']
         return project
-        
+
+
 async def get_data_asynchronous(headers=None):
     with ThreadPoolExecutor(max_workers=10) as executor:
         with requests.Session() as session:
@@ -49,11 +56,17 @@ async def get_data_asynchronous(headers=None):
             tasks = [
                 loop.run_in_executor(
                     executor,
-                    functools.partial(fetch_one, session, project, headers=headers)
+                    functools.partial(
+                        fetch_one,
+                        session,
+                        project,
+                        headers=headers
+                    )
                 )
                 for project in projects_we_want
             ]
             return await asyncio.gather(*tasks)
+
 
 def download_file(url, file_save_path):
     with open(file_save_path, 'wb') as f, requests.get(url, stream=True) as r:
@@ -69,13 +82,22 @@ def download_file(url, file_save_path):
                 download_progress += len(chunk)
                 f.write(chunk)
                 percent_done = download_progress / total_length
-                progress_trackers_complete = int(num_progress_trackers * percent_done)
+                progress_trackers_complete = int(
+                    num_progress_trackers * percent_done
+                )
                 done = '=' * progress_trackers_complete
-                not_done =  ' ' * (num_progress_trackers-progress_trackers_complete)
-                
-                print(f'[{done}{not_done}] {percent_done:.2%}',end='\r',flush=True)
-        print() # escape carridge escaped line
+                not_done = ' ' * (
+                    num_progress_trackers - progress_trackers_complete
+                )
+
+                print(
+                    f'[{done}{not_done}] {percent_done:.2%}',
+                    end='\r',
+                    flush=True
+                )
+        print()  # escape carridge escaped line
     return (time.perf_counter() - start)
+
 
 def get_installed_versions():
     with open(downloaded_files_info) as f:
@@ -84,7 +106,12 @@ def get_installed_versions():
     # if it does, add the installed version to projects_we_want
     for project in apparent_downloaded:
         if Path(downloads_dir, project['asset_name']).exists():
-            if remote_proj:=next((item for item in projects_we_want if item['id'] == project['id']), None):
+            if remote_proj := next((
+                    item
+                    for item in projects_we_want
+                    if item['id'] == project['id']
+                ), None
+            ):
                 remote_proj['installed_ver'] = project['installed_ver']
 
 
@@ -97,24 +124,34 @@ if __name__ == '__main__':
 
     # Get current versions asynchronously
     loop = asyncio.get_event_loop()
-    verion_numbers_future = asyncio.ensure_future(get_data_asynchronous(
-        headers={'Accept': 'application/json'}
-    ))
-    projects_we_want = loop.run_until_complete(verion_numbers_future)
+    projects_we_want = loop.run_until_complete(
+        get_data_asynchronous(headers={'Accept': 'application/json'})
+    )
 
     for project in projects_we_want:
-        print(f'Latest version of {project["project"]} is {project["latest_version"]}')
+        print(
+            f'Latest version of {project["project"]} is '
+            f'{project["latest_version"]}'
+        )
 
         if project.get('installed_ver', None) == project["latest_version"]:
-            print(f'Latest version already installed, skipping...')
+            print('Latest version already installed, skipping...')
         else:
             file_save_path = Path(downloads_dir, project['asset_name'])
             print(f'Downloading {project["asset_name"]}')
-            time_elapsed = download_file(project['asset_download'], file_save_path)
-            print(f'Finished downloading. Time taken: {round(time_elapsed, 2)}')
+            time_elapsed = download_file(
+                project['asset_download'],
+                file_save_path
+            )
+            print(
+                'Finished downloading. Time taken: '
+                f'{round(time_elapsed, 2)}'
+            )
 
-        print() # Seperation
-        del project["latest_version"] # Remove this so that it isn't included in local file, causing confusion
-    
+        print()  # Seperation
+
+        # Remove this so that it isn't included in local file causing confusion
+        del project["latest_version"]
+
     with open(downloaded_files_info, 'w') as f:
         json.dump(projects_we_want, f, indent=4)
